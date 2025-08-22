8eba4575486b478a5419c202fc557032e5a39ed0
John Duggan <dugganjw@ornl.gov>
2025-08-22 16:37:57 -0400

    Fix incorrect warning when omitting facility parameter to NeutronDataSelector

--- src/nova/trame/view/components/ornl/neutron_data_selector.py
+++ src/nova/trame/view/components/ornl/neutron_data_selector.py
@@ -91,7 +91,7 @@ class NeutronDataSelector(DataSelector):
         if data_source == "oncat" and subdirectory:
             warn("subdirectory will be ignored since data will be pulled from ONCat.", stacklevel=1)
 
-        if isinstance(facility, str) and allow_custom_directories:
+        if isinstance(facility, str) and facility and allow_custom_directories:
             warn("allow_custom_directories will be ignored since the facility parameter is fixed.", stacklevel=1)
 
         self._facility = facility
